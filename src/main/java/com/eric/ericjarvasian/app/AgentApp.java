eb817511f15bbcd453cb72ea497fa107a1283f40
vegetabledog5058 <505831532@qq.com>
2025-06-09 21:45:47 +0800

    1. 添加文件chatMemory 2. 实现prompt,entity特性 3. 新增依赖

--- src/main/java/com/eric/ericjarvasian/app/AgentApp.java
+++ src/main/java/com/eric/ericjarvasian/app/AgentApp.java
@@ -1,43 +1,114 @@
 package com.eric.ericjarvasian.app;
 
 import com.eric.ericjarvasian.advisor.MyLoggerAdvisor;
-import com.eric.ericjarvasian.advisor.ReReadingAdvisor;
+import com.eric.ericjarvasian.chatmemory.FileChatMemory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.model.Generation;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_RETRIEVE_SIZE_KEY;
 
 @Component
 @Slf4j
 public class AgentApp {
-
+    @Autowired
+    private ChatModel dashscopeChatModel;
     private final ChatClient chatClient;
 
     public AgentApp(ChatModel dashscopeChatModel) {
-        ChatMemory chatMemory = new InMemoryChatMemory();
+//        ChatMemory chatMemory = new InMemoryChatMemory();
+        String directory = System.getProperty("user.dir") + "/tem/chat_memory";
+        ChatMemory chatMemory = new FileChatMemory(directory);
         chatClient = ChatClient.builder(dashscopeChatModel)
-                .defaultAdvisors(new MessageChatMemoryAdvisor(chatMemory),
-                        new MyLoggerAdvisor(),
+                .defaultAdvisors(
+                        new MessageChatMemoryAdvisor(chatMemory),
+                        new MyLoggerAdvisor()
                         //reReadingAdvisor
-                        new ReReadingAdvisor()
+//                        new ReReadingAdvisor()
 
                 ).build();
     }
 
+    record placeList(String place, List<String> content) {
+    }
+
     public String chat(String message, String conversantId) {
+
+
         ChatResponse chatResponse = chatClient.prompt()
 //                .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, conversantId)
 //                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
-
                 .user(message)
                 .call()
                 .chatResponse();
         String content = chatResponse.getResult().getOutput().getText();
-
         return content;
+
+    }
+
+    //    prompt示例
+    public List<Generation> chatForPrompt(String message, String conversantId) {
+        //        prompt示例1
+//        PromptTemplate promptTemplate = new PromptTemplate("Tell me {num} joke about {topic}");
+//        Prompt prompt = promptTemplate.create(Map.of("num", "1", "topic", "weather"));
+//        prompt示例2
+        String userText = """
+                Tell me about three famous pirates from the Golden Age of Piracy and why they did.
+                Write at least a sentence for each pirate.
+                """;
+
+        Message userMessage = new UserMessage(userText);
+
+        String systemText = """
+                You are a helpful AI assistant that helps people find information.
+                Your name is {name}
+                You should reply to the user's request with your name and also in the style of a {voice}.
+                """;
+
+        SystemPromptTemplate systemPromptTemplate = new SystemPromptTemplate(systemText);
+        Message systemMessage = systemPromptTemplate.createMessage(Map.of("name", "jarvasian", "voice", "warmly"));
+
+        Prompt prompt = new Prompt(List.of(userMessage, systemMessage));
+
+        List<Generation> results = dashscopeChatModel.call(prompt).getResults();
+        log.info("results: {}", results);
+
+        List<Generation> response = chatClient.prompt(prompt)
+                .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, conversantId)
+                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
+                .user(message)
+                .call()
+
+                .chatResponse()
+                .getResults();
+        return response;
+    }
+
+
+    public placeList chatToRetBean(String message, String conversantId) {
+        placeList chatResponse = chatClient.prompt()
+//                .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, conversantId)
+//                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
+                .system("每次对话后生成地名，具体游玩地方")
+                .user(message)
+                .call()
+                .entity(placeList.class);
+
+
+        return chatResponse;
     }
 }
--- src/test/java/com/eric/ericjarvasian/app/AgentAppTest.java
+++ src/test/java/com/eric/ericjarvasian/app/AgentAppTest.java
@@ -2,24 +2,40 @@ package com.eric.ericjarvasian.app;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.ai.chat.model.Generation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
 @Slf4j
 class AgentAppTest {
 
     @Autowired
     private AgentApp agentApp;
+
     @Test
     void chat() {
         String conversantId = UUID.randomUUID().toString();
         String response = agentApp.chat("你好,你是谁?", conversantId);
 
+        assertNotNull(response);
+    }
+
+    @Test
+    void chatToRetBean() {
+        AgentApp.placeList response = agentApp.chatToRetBean("给我南京最受欢迎的3个游玩好去处", null);
+        assertNotNull(response);
+    }
+
+    @Test
+    void chatForPrompt() {
+        String conversantId = UUID.randomUUID().toString();
+        List<Generation> response = agentApp.chatForPrompt("hello", conversantId);
         assertNotNull(response);
     }
 }
